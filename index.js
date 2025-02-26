7aa3fe5b6a1563a9ba377f0270e84ab998e952d7
arifjamal90 <mohdarif727431@gmail.com>
2025-02-26 13:31:46 +0530

    add cors

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const server = http.createServer(app);
 const io = initializeSocket(server);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000", 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+}));
 
 // Import Routes
 const authRoutes = require("./routes/authRoutes");
